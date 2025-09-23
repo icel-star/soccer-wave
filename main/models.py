d46bff342ac3a43f14fe16f4d25b8329ce44d55b
icel-star <giselle.julia@ui.ac.id>
2025-09-23 21:37:53 +0700

    Tugas 4 code done

--- main/models.py
+++ main/models.py
@@ -1,8 +1,10 @@
 import uuid
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    
     CATEGORY_CHOICES = [
         ('jersey', 'Jersey'),
         ('shoes', 'Shoes'),
@@ -16,7 +18,7 @@ class Product(models.Model):
     rating = models.FloatField(default=0.0)
     brand = models.CharField(max_length=100, blank=True, null=True)
     thumbnail = models.URLField()
-    category = models.CharField(max_length=100)
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES)
     stock = models.IntegerField()
     product_views = models.IntegerField(default=0)
     is_featured = models.BooleanField(default=False)
